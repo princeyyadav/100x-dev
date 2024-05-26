bc3b092185c14be2814debfc78bd8ea80ecfef6f
princeyyadav <princey.y@perfios.com>
2024-05-26 12:56:08 +0530

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # 100x-dev
 Code base for 100x-dev course 
+
+## Week 1
+### Learning Javascript
+- [x] 1.4  
+- [ ] 1.5 Async vs sync functions, real use of callbacks, JS browser architecture, Promises, async await
---
+++ classes/week-1/sync_async.js
@@ -0,0 +1,34 @@
+// synchronous
+/*
+one after other, sequential, only one thing is happening at a time
+
+Asynchronous
+Happens in parts
+Multiple things are context switching with others
+
+Async fns vs async fns
+
+Some intuition:
+Human body is single threaded:
+we can only do one task at a time
+But we can context switch between tasks or we can delegate tasks to other people
+
+Same thing is with JS
+JS is single threaded
+
+Make maggie:
+Boil water
+Cut maggie packet
+Chop veggies
+Buy catchup from market
+
+Sequential way: one thing at a time (37m)
+
+Async way: 12m
+Boil water
+Delegates buying ketchup
+Cut maggie packet
+Chops veggies
+
+*/
+
